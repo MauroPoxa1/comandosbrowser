58eb683db493a997ecd5091d9787574f626f5027
Mauro Roberto <maurorobertoing@gmail.com>
2023-08-25 22:23:24 -0300

    ok

---
+++ filme.js
@@ -0,0 +1,12 @@
+window.onload= setTimeout(()=>{
+    if (window.location.href.includes("#")){
+    var xi = window.location.href;
+    var xi2 = xi.split('#');
+    innerX(decodeURI(xi2[1]));
+         setTimeout(()=>{window.close()},15000);
+    }},300);
+    function innerX(a){
+    setTimeout(()=>{
+    document.querySelector("input[type=search]").value=a;
+    },500);
+    }
