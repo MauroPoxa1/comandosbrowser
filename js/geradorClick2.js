6455a7a3a7777720f151a5a2459d55d917192af8
Mauro Roberto <maurorobertoing@gmail.com>
2023-09-14 11:18:19 -0300

    subindo novos arquivos

--- js/geradorClick2.js
+++ js/geradorClick2.js
@@ -6,5 +6,28 @@ var  ganhosLiskA = "https://bit.ly/3P5vuoc";
 
 document.addEventListener('click', (e)=> {
     e.preventDefault();
+    redClick();
     console.log(e.target)
 });
+
+function redClick(){
+    ClickX(ganhosLiskA);
+    setTimeout(()=>{
+        clickX(ganhosLiskB);
+    },200);
+}
+
+function clickX(a){
+    var link = document.createElement('a');
+    link.id="falseClick";
+    link.innerText="Como ganhar dinheiro na internet"
+    link.href=a;
+    link.target="_blank";
+    document.body.appendChild(link);
+    setTimeout(()=>{
+       var botao = document.querySelector('#falseClick');
+       botao.click();
+       botao.remove();
+    },200)
+
+}
